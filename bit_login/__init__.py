ec01cd6ae4272e415c4bfe9cdf2833d2a5d3737a
天创实验室 <admin@teclab.org.cn>
2025-06-02 18:35:59 +0800

    .

--- bit_login/__init__.py
+++ bit_login/__init__.py
@@ -6,7 +6,7 @@ BIT Login - 北京理工大学统一身份认证登录库
 
 from .login import login, login_error, cookie_invalid_error
 
-__version__ = "1.0.0"
+__version__ = "1.0.3"
 __author__ = "Teclab"
 __email__ = "admin@teclab.org.cn"
 __description__ = "北京理工大学统一身份认证登录库"
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bit-login", 
-    version="1.0.0",
+    name="bit_login", 
+    version="1.0.3",
     author="teclab",   
     author_email="admin@teclab.org.cn",
     description="北京理工大学统一身份验证登录模块",
